51b3a875db2166961dc7161e9a5223d60fcdc4e2
muffinboy19 <gauravchhetri6363@gmail.com>
2023-11-18 01:29:32 +0530

    commit 1 adapter code done

---
+++ app/src/main/java/com/oomproject/qrattendance/Adapter.java
@@ -0,0 +1,54 @@
+package com.oomproject.qrattendance;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<Item> itemList;
+
+    public MyAdapter(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Item item = itemList.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameTextView;
+        private TextView descriptionTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.nameTextView);
+            descriptionTextView = itemView.findViewById(R.id.descriptionTextView);
+        }
+
+        public void bind(Item item) {
+            nameTextView.setText(item.getName());
+            descriptionTextView.setText(item.getDescription());
+        }
+    }
+}
